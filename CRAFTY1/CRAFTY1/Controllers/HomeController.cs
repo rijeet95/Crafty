f1b367d2161fc4ced35c126d36c7194bfb41170b
Rijeet Bin Anis <59135011+rijeet@users.noreply.github.com>
2022-08-01 01:03:07 +0600

    Update

--- CRAFTY1/CRAFTY1/Controllers/HomeController.cs
+++ CRAFTY1/CRAFTY1/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace CRAFTY1.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Contact Rijeet Bin Anis bhai";
+            ViewBag.Message = "Contact Page.";
 
             return View();
         }
